24b094ed1bd521003b6535d898b1615bc4d9c9ec
Jono Prest <jjprest@gmail.com>
2024-05-24 17:30:06 +0200

    Safely handle exn

--- src/InjectedForLoop.js
+++ src/InjectedForLoop.js
@@ -3,10 +3,10 @@ export const forOf = (iterator, fn) => {
     try {
       fn(body);
     } catch (exn) {
-      if (exn.RE_EXN_ID.includes("Iterator.Break")) {
+      if (exn?.RE_EXN_ID?.includes("Iterator.Break")) {
         break;
       }
-      if (exn.RE_EXN_ID.includes("Iterator.Continue")) {
+      if (exn?.RE_EXN_ID?.includes("Iterator.Continue")) {
         continue;
       }
       throw exn;
